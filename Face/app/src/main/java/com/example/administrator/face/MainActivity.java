f0a825a7f14e77625655200f64d1dfb1c636b9f3
lishutong61 <lishutong61@outlook.com>
2018-03-07 15:00:15 +0800

    增加了记住密码功能“
    '

--- Face/app/src/main/java/com/example/administrator/face/MainActivity.java
+++ Face/app/src/main/java/com/example/administrator/face/MainActivity.java
@@ -2,40 +2,68 @@ package com.example.administrator.face;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-    private EditText username;
-    private EditText password;
+    private EditText usernameEdit;
+    private EditText passwordEdit;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Button login;
+    private CheckBox remeberPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button=(Button) findViewById(R.id.button_1);
-        username=(EditText) findViewById(R.id.editText);
-        password=(EditText) findViewById(R.id.editText2);
-        button.setOnClickListener(new View.OnClickListener(){
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        remeberPass=(CheckBox) findViewById(R.id.remember_pass);
+        login=(Button) findViewById(R.id.button_1);
+        usernameEdit=(EditText) findViewById(R.id.editText);
+        passwordEdit=(EditText) findViewById(R.id.editText2);
+        boolean isRemeber=pref.getBoolean("remember_password",false);
+
+        if(isRemeber){
+            //将账号和密码都设置到文本框中
+            String username=pref.getString("username","");
+            String password=pref.getString("password","");
+            usernameEdit.setText(username);
+            passwordEdit.setText(password);
+            remeberPass.setChecked(true);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                switch(v.getId()){
+            public void onClick(View v) {
+                switch (v.getId()) {
                     case R.id.button_1:
-                        String username1=username.getText().toString();
-                        String passowrd1=password.getText().toString();
-                        int password2=Integer.parseInt(passowrd1);
-                        if(username1.equals("hhh") && password2==321){
-                            Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
-                            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                        String username1 = usernameEdit.getText().toString();
+                        String password1 = passwordEdit.getText().toString();
+                        if (username1.equals("hhh") && password1.equals("321")) {
+                            editor=pref.edit();
+
+                            if(remeberPass.isChecked()){
+                                editor.putBoolean("remember_password",true);
+                                editor.putString("username",username1);
+                                editor.putString("password",password1);
+                            }else{
+                                editor.clear();
+                            }
+                            editor.apply();
+                            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                             startActivity(intent);
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
                         }
                         break;
                     default:
